ed4a2bace790f0531b83cc8b1456db25e76a2b46
Gouki.ET <scubillosq@gmail.com>
2023-04-03 20:30:31 -0400

    Se crea el esquema User

--- models/User.js
+++ models/User.js
@@ -1,1 +1,38 @@
 const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        default: "Nombre no ingresado",
+        trim: true,
+        lowercase: true,
+        minLength: 2
+    },
+    lastName:{
+        type: String,
+        default: "Apellido no ingresado",
+        trim: true,
+        lowercase: true,
+        minLength:2
+    },
+    email: {
+        type: String,
+        trim: true,
+        match: [/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g],
+        required: true
+    },
+    age: {
+        type: Number,
+        min: 16,
+        max: 120
+    },
+    password: {
+        type: String,
+        match: [/^(?=.*\d)(?=.*[a-z])(?=.*[a-zA-Z]).{8,}$/gm],
+        required: true
+    }
+})
+
+const User = mongoose.model('user', userSchema);
+
+module.exports = User;
