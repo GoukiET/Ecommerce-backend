de97ac955440d5322519a17ac34002078726cb01
Gouki.ET <scubillosq@gmail.com>
2023-04-03 22:37:10 -0400

    Se crea la funcion editUser

--- controllers/userController.js
+++ controllers/userController.js
@@ -18,5 +18,17 @@ const getUsers = async(req, res) => {
     } catch (error) {
         res.json({ success: false, message: error.message });
     }
-}
-module.exports = {createUser, getUsers};
+};
+
+const editUser = async(req, res) => {
+   
+    try {
+        const {id} = req.params;
+        const contain = req.body;
+        const updateUser = await User.findByIdAndUpdate(id, contain, {new: true});
+        res.json({success: true, message: "Usuario Actualizado", updateUser}) 
+    } catch (error) {
+        res.status(500).json({success: false, message: error.message})
+    }
+};
+module.exports = {createUser, getUsers, editUser};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {createUser, getUsers} = require('../controllers/userController');
+const {createUser, getUsers, editUser} = require('../controllers/userController');
 
 const userRouter = express.Router();
 
@@ -8,6 +8,6 @@ userRouter.route('/user')
     .get(getUsers)
 
 userRouter.route('/user/:id')
-    .put()
+    .put(editUser)
     .get()
 module.exports = userRouter;
