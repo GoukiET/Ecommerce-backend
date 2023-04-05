50174bb56cb2e098117d1c04695d1e7e02386049
Sebastian <scubillosq@gmail.com>
2023-04-05 02:31:39 -0400

    Feature creacion de Product Schema

---
+++ models/Product.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    SKU: {
+        type: String,
+        required: true,
+        minLength: 5,
+        maxLength: 5
+    },
+    albumName: {
+        type: String,
+        required: true,
+        uppercase: true,
+        trim: true
+    },
+    bandName: {
+        type: String,
+        required: true,
+        uppercase: true,
+        trim: true
+    },
+    price: {
+        type: Number,
+        require: true,
+        min: 0
+    },
+    format: {
+        type: String,
+        require: true,
+        uppercase: true
+    },
+    stock: {
+        type: Number,
+        require: true,
+        min: 0
+    }
+})
+
+const Product = mongoose.model("product", productSchema);
