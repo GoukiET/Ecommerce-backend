966fb53a6dbaa0764a86dd2f9aea1737b55b4232
Sebastian <scubillosq@gmail.com>
2023-04-09 21:49:11 -0400

    Feature add comentario

--- models/User.js
+++ models/User.js
@@ -55,4 +55,4 @@ userSchema.methods.hashValidation = function(password, salt, passwordDB){
 
 const User = mongoose.model('user', userSchema);
 
-module.exports = User;
+module.exports = User; //Hola
