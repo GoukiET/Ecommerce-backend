8f4577c494bb01e598760738e918da5fbd9120fc
Gouki.ET <scubillosq@gmail.com>
2023-04-14 00:33:35 -0400

    Feature se añade al servidor la constante cors y se añade al middleware para su uso

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const userRouter = require('./routes/userRoutes');
 const productRouter = require('./routes/productRoutes');
+const cors = require('cors');
 
 require('dotenv').config();
 require('./config/database');
 const app =  express();
 
 //* Middleware
+app.use(cors());
 app.use(express.json());
 app.use(userRouter);
 app.use(productRouter);
